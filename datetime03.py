ec5164d8806e24d0668c21f5a7d7491ac2121d59
Cryhelyxx <929679459@qq.com>
2014-04-19 02:10:54 +0800

    python-spider

---
+++ datetime03.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python
+#-*- coding: utf-8 -*-
+#file: datetime03.py
+"""
+	计算上一个的时间
+"""
+#引入datetime,calendar两个模块
+import datetime,calendar
+  
+last_friday = datetime.date.today() 
+oneday = datetime.timedelta(days = 1) 
+    
+while last_friday.weekday() != calendar.FRIDAY: 
+    last_friday -= oneday 
+    
+print(last_friday.strftime('%A, %d-%b-%Y'))
