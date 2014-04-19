472071a52ac6bf49a77c3a2b5cd8373b545f6da2
Cryhelyxx <929679459@qq.com>
2014-04-19 13:45:08 +0800

    python-spider

--- connMySQLDB.py
+++ connMySQLDB.py
@@ -1,22 +1,27 @@
-#-*- coding:utf-8 -*-
+#-*- coding: utf-8 -*-
+#file: connMySQLDB.py
 import os, sys, string
-import MySQLdb
+import MySQLdb #导入MySQLdb模块
 
-conn = MySQLdb.connect(host = 'localhost', user = 'root', passwd = 'admin',db = 'user')
+conn = MySQLdb.connect(
+	host = 'localhost',       #连接到数据库， 服务器为本机 
+	user = 'root',            #用户名为root
+	passwd = 'admin',         #密码为admin
+	db = 'user')              #数据库名为user
 
-cursor = conn.cursor()
+cursor = conn.cursor()        #获取数据库游标
+#sql插入语句
 sql = "insert into telephone(name, telephone) values ('%s', %d)" % ("Cryhelyx", 10086)
 
-cursor.execute(sql)
-
+cursor.execute(sql)           #执行sql插入语句
+#sql查询语句
 sql = "select * from telephone"
-cursor.execute(sql)
-alldata = cursor.fetchall()
-
+cursor.execute(sql)           #执行sql查询语句
+alldata = cursor.fetchall()   #接收全部的返回结果行
+#打印每条记录行
 if alldata:
     for rec in alldata:
         print rec[0], rec[1]
-
-cursor.close()
-
-conn.close()
+#关闭数据库连接, 需要分别的关闭指针对象和连接对象
+cursor.close()                #关闭游标
+conn.close()                  #关闭数据库
